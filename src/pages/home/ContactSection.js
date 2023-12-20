4653f8d7e07ef4274995e0bb4cb6bc0af966af7f
unknown <spartha343@gmail.com>
2023-12-21 02:25:32 +0600

    contact section link added

--- src/pages/home/ContactSection.js
+++ src/pages/home/ContactSection.js
@@ -11,11 +11,11 @@ import ExploreButton from './ExploreButton';
 const ContactSection = () => {
 
     const contactInfos = [
-        { itemName: 'Facebook', itemIcon: <FaFacebook size={40} /> },
+        { itemName: 'Facebook', itemIcon: <FaFacebook size={40} />, link: 'https://www.facebook.com/partho.debnath343' },
         { itemName: 'Instagram', itemIcon: <FiInstagram size={40} />, bgColor: 'bg-gradient-to-r from-secondary to-primary' },
-        { itemName: "What's App", itemIcon: <BsWhatsapp size={40} /> },
-        { itemName: 'Email', itemIcon: <MdOutlineMailOutline size={40} />, bgColor: 'bg-gradient-to-r from-secondary to-primary' },
-        { itemName: 'Phone', itemIcon: <FiPhoneCall size={40} /> },
+        { itemName: "What's App", itemIcon: <BsWhatsapp size={40} />, link: 'https://api.whatsapp.com/send/?phone=%2B8801624457470&text&type=phone_number&app_absent=0' },
+        { itemName: 'Email', itemIcon: <MdOutlineMailOutline size={40} />, link: 'mailto:spartha343@gmail.com', bgColor: 'bg-gradient-to-r from-secondary to-primary' },
+        { itemName: 'Phone', itemIcon: <FiPhoneCall size={40} />, link: 'tel:008801624457470' },
     ];
 
     return (
