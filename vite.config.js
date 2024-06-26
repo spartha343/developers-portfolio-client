1002c69059d70883e17429b73f659316e5e4ec6a
partha <spartha343@gmail.com>
2024-06-26 15:49:25 +0600

    vite config changed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: "build" // Specify the output directory for the production build
+    // You can optionally configure other build settings here
+    // For example, if you need to prefix the assets path for deployment
+    // assetsDir: 'assets',
+    // or if you need to configure chunk splitting or minification:
+    // terserOptions: {},
+    // chunkSizeWarningLimit: 500, // Adjust according to your project's needs
+  }
+});
